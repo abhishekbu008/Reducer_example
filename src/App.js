0e9b74d5d45d6b7d2c30cfd0728a60d7d8e16a1b
abhishekbu008 <80893354+abhishekbu008@users.noreply.github.com>
2021-08-14 16:02:01 +0530

    added custom reducer for async

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import { useReducer } from "react";
+import axios from "axios";
 import "./styles.css";
 
 const ACTIONS = {
   INCREMENT: "INCREMENT",
   DECREMENT: "DECREMENT",
-  RESET: "RESET"
+  RESET: "RESET",
+  FETCHDATA: "FETCH_DATA"
+};
+
+// custom reducer hook with async dispatch
+const useReducerWithThunk = (reducer, initialState) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const customDispatch = (action) => {
+    if (typeof action === "function") {
+      action(customDispatch);
+    } else {
+      dispatch(action);
+    }
+  };
+
+  return [state, customDispatch];
 };
 
 // State file
-const incrementCounter = (state, action) => ({ count: state.count + 1 });
-const decrementCounter = (state, action) => ({ count: state.count - 1 });
-const resetCounter = (state, action) => ({ count: action.payload.count });
+const incrementCounter = (state, action) => ({
+  ...state,
+  count: state.count + 1
+});
+const decrementCounter = (state, action) => ({
+  ...state,
+  count: state.count - 1
+});
+const resetCounter = (state, action) => ({
+  ...state,
+  count: action.payload.count
+});
+
+const updateData = (state, action) => ({ ...state, data: action.payload });
 
 const initialState = {
-  count: 0
+  count: 0,
+  data: {}
 };
 
 function reducer(state, action) {
@@ -24,18 +53,34 @@ function reducer(state, action) {
       return decrementCounter(state, action);
     case ACTIONS.RESET:
       return resetCounter(state, action);
+    case ACTIONS.FETCHDATA:
+      return updateData(state, action);
     default:
       return state;
   }
 }
 
+const axiosInstance = axios.create();
+
+const fetchData = () => async (dispatch) => {
+  const post = await axiosInstance.get(
+    "https://jsonplaceholder.typicode.com/posts/1"
+  );
+  console.log(post);
+  dispatch({ type: ACTIONS.FETCHDATA, payload: post.data });
+};
+
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducerWithThunk(reducer, initialState);
 
   const onIncrementClick = () => dispatch({ type: ACTIONS.INCREMENT });
   const onDecrementClick = () => dispatch({ type: ACTIONS.DECREMENT });
   const onResetCounter = () =>
     dispatch({ type: ACTIONS.RESET, payload: initialState });
+
+  const getData = () => {
+    dispatch(fetchData());
+  };
 
   return (
     <>
@@ -50,7 +95,11 @@ export default function App() {
         <button style={{ marginRight: "10px" }} onClick={onResetCounter}>
           Reset
         </button>
+        <button style={{ marginRight: "10px" }} onClick={getData}>
+          Get Data
+        </button>
       </div>
+      <div>Data: {JSON.stringify(state.data)}</div>
     </>
   );
 }
